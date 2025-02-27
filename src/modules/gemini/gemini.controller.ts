8b7f20c73b31aa26b546bdaab758e3e0f751fbef
Mtri nèeeeeee <90810549+mtrine@users.noreply.github.com>
2025-02-27 09:51:59 +0700

    cancel public  in gemini controller

--- src/modules/gemini/gemini.controller.ts
+++ src/modules/gemini/gemini.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
 import { GetAiMessageDto } from './dto/get-ai-message.dto';
-import { Public } from 'src/decorators/public.decorator';
 import { ResponseMessage } from 'src/decorators/response-message.decorator';
 
 @Controller('gemini')
@@ -9,11 +8,9 @@ export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
 
   @Post()
-  @Public()
   @ResponseMessage('Get AI message successfully')
   async getAiMessage(@Body() dto:GetAiMessageDto){
     const result = await this.geminiService.generateText(dto);
     return result;
-
   }
 }
